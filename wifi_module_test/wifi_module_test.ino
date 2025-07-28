75d70fa28db4cc0e8c7047c745c43c228ff63e13
User-Suuro <godwingalvez26@gmail.com>
2025-07-28 16:55:41 +0800

    added wifi_module_test

---
+++ wifi_module_test/wifi_module_test.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+
+// Important: Set your Serial Monitor Settings Line Ending to "Both NL & CR"
+// Pins: RX (Arduino receives from ESP TX), TX (Arduino sends to ESP RX)
+
+SoftwareSerial espSerial(3,2); // RX, TX
+
+void setup() {
+  Serial.begin(9600);       // Monitor Serial
+  espSerial.begin(9600);    // ESP-01S default baud rate
+
+  delay(2000);
+  Serial.println("ESP-01S AT Command Test Ready");
+}
+
+void loop() {
+  // Forward data from ESP to Serial Monitor
+  if (espSerial.available()) {
+    Serial.write(espSerial.read());
+  }
+
+  // Forward data from Serial Monitor to ESP
+  if (Serial.available()) {
+    espSerial.write(Serial.read());
+  }
+
+  // Type AT in the Serial Monitor for Testing, it should return "OK"
+}
